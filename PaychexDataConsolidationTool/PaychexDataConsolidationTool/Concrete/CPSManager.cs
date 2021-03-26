e8c62f73be070e86c0c12b95d34a6b66487e9494
Mike_Kowulich <Mike_Kowulich@bitbucket.org>
2021-03-25 22:46:09 -0400

    Graph CPS 1.0

--- PaychexDataConsolidationTool/PaychexDataConsolidationTool/Concrete/CPSManager.cs
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/Concrete/CPSManager.cs
@@ -50,25 +50,87 @@ namespace PaychexDataConsolidationTool.Concrete
             return totCPS;
         }
 
-        public Task<List<CPS>> ListAll(int skip, int take, string orderBy, string direction = "DESC", string search = "")
+        public Task<List<CPS>> ListAll(string orderBy, string direction = "DESC", string search = "")
         {
             var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
-                ($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date like '%{search}%' ORDER BY {orderBy} {direction} OFFSET {skip} ROWS FETCH NEXT {take} ROWS ONLY; ", null, commandType: CommandType.Text));
+                ($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date like '%{search}%' ORDER BY {orderBy} {direction};", null, commandType: CommandType.Text));
             return cpss;
         }
-        public Task<List<CPS>> SearchDates(int skip, int take, string orderBy, string startDate, string endDate, string direction = "DESC")
+        public Task<List<CPS>> SearchDates(string orderBy, string startDate, string endDate, string direction = "DESC")
         {
             var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
-                ($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY {orderBy} {direction} OFFSET {skip} ROWS FETCH NEXT {take} ROWS ONLY; ", null, commandType: CommandType.Text));
-            Console.WriteLine($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY {orderBy} {direction} OFFSET {skip} ROWS FETCH NEXT {take} ROWS ONLY;");
+                ($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY {orderBy} {direction}; ", null, commandType: CommandType.Text));
+           // Console.WriteLine($"SELECT FORMAT (Date, 'yyyy-MM-dd') as Date, Status, Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY {orderBy} {direction} OFFSET {skip} ROWS FETCH NEXT {take} ROWS ONLY;");
             return cpss;
         }
         public Task<int> CountAfterSearch(string startDate, string endDate)
         {
             var totCPS = Task.FromResult(_dapperManager.Get<int>($"select COUNT(*) from [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}'", null,
                     commandType: CommandType.Text));
-            Console.WriteLine($"select COUNT(*) from [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}'");
+            //Console.WriteLine($"select COUNT(*) from [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}'");
             return totCPS;
+        }
+
+        public Task<List<CPS>> getDates(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT DISTINCT FORMAT (Date, 'yyyy-MM-dd') as Date FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY Date ASC", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT DISTINCT FORMAT (Date, 'yyyy-MM-dd') as Date FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' ORDER BY Date ASC");
+            return cpss;
+        }
+
+        public Task<List<CPS>> getInactives(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Inactive' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Inactive' ORDER BY Date ASC");
+            return cpss;
+        }
+        public Task<List<CPS>> getActives(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Active' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Active' ORDER BY Date ASC");
+            return cpss;
+        }
+
+        public Task<List<CPS>> getDemos(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Demo' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Demo' ORDER BY Date ASC");
+            return cpss;
+        }
+
+        public Task<List<CPS>> getMasters(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Master' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Master' ORDER BY Date ASC");
+            return cpss;
+        }
+
+        public Task<List<CPS>> getSuspendeds(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Suspended' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Suspended' ORDER BY Date ASC");
+            return cpss;
+        }
+
+        public Task<List<CPS>> getDeleteds(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Deleted' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Deleted' ORDER BY Date ASC");
+            return cpss;
+        }
+        public Task<List<CPS>> getImplementations(string startDate, string endDate)
+        {
+            var cpss = Task.FromResult(_dapperManager.GetAll<CPS>
+                ($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Implementation' ORDER BY Date ASC;", null, commandType: CommandType.Text));
+            Console.WriteLine($"SELECT Total FROM [CPS] WHERE Date >= '{startDate}' AND Date <= '{endDate}' AND Status='Implementation' ORDER BY Date ASC");
+            return cpss;
         }
         public Task<int> Update(CPS cps)
         {
--- PaychexDataConsolidationTool/PaychexDataConsolidationTool/wwwroot/js/interop.js
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/wwwroot/js/interop.js
@@ -1,4 +1,194 @@
-﻿function generateCPSGraph(){
-    alert("Hello");
+﻿function generateCPSGraph(graphdata, InactiveData, ActiveData, MasterData, DemoData, SuspendedData, DeletedData, ImplementationData){
+    var canvas = document.getElementById("lineGraph");
+    var ctx = canvas.getContext('2d');
+    Chart.defaults.global.defaultFontColor = 'black';
+    Chart.defaults.global.defaultFontSize = 16;
+
+    console.dir(JSON.stringify(graphdata));
+    var data = {
+        labels: graphdata.dates,
+        datasets: [{
+            label: "Inactive",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(225,0,0,0.4)",
+            borderColor: "red", // The main line color
+            borderCapStyle: 'square',
+            borderDash: [], // try [5, 15] for instance
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "black",
+            pointBackgroundColor: "white",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "yellow",
+            pointHoverBorderColor: "brown",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: true
+            data: graphdata.inactives,
+            spanGaps: true,
+        }, {
+            label: "Active",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(0, 200, 0)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.actives,
+            spanGaps: false,
+            }, {
+            label: "Demo",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(227, 203, 20)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.demos,
+            spanGaps: false,
+            }, {
+            label: "Master",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(0, 0, 255)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.masters,
+            spanGaps: false,
+            }, {
+            label: "Suspended",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(128, 128, 0)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.suspendeds,
+            spanGaps: false,
+            },
+            {
+            label: "Deleted",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(0, 0, 0)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.deleteds,
+            spanGaps: false,
+            }, {
+            label: "Implementation",
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: "rgba(167,105,0,0.4)",
+            borderColor: "rgb(255, 102, 0)",
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: "white",
+            pointBackgroundColor: "black",
+            pointBorderWidth: 1,
+            pointHoverRadius: 8,
+            pointHoverBackgroundColor: "brown",
+            pointHoverBorderColor: "yellow",
+            pointHoverBorderWidth: 2,
+            pointRadius: 4,
+            pointHitRadius: 10,
+            // notice the gap in the data and the spanGaps: false
+            data: graphdata.implementations,
+            spanGaps: false,
+        },
+
+
+        ]
+    };
+
+    // Notice the scaleLabel at the same level as Ticks
+    var options = {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                },
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Count',
+                    fontSize: 20
+                }
+            }]
+        }
+    };
+
+    // Chart declaration:
+    var myBarChart = new Chart(ctx, {
+        type: 'line',
+        data: data,
+        options: options
+    });
     return true;
 }

c7d2caa7613fa0c0158ddd445d77832f5841f89d
ianfarre11 <ifarr1@brockport.edu>
2021-04-14 18:59:56 -0400

    Created Models and Entities to be used for UsersPerType page.

---
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/Entities/UPTType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaychexDataConsolidationTool.Entities
+{
+    public class UPTType
+    {
+        [Key]
+        public int UserPerTypeId { get; set; }
+
+        public string DateOfReport { get; set; }
+
+        public string UserTypeId { get; set; }
+
+        public int TypeCountAsOfDate { get; set; }
+
+        public string UserTypeName { get; set; }
+    }
+}
---
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/Entities/UserType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PaychexDataConsolidationTool.Entities
+{
+    public class UserType
+    {
+        [Key]
+        public int UserTypeId { get; set; }
+
+        public string UserTypeName { get; set; }
+
+    }
+}

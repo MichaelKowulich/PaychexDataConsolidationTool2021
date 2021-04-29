849de562cc2c067f2f293c85104cdd8c0e1805a1
Mike_Kowulich <Mike_Kowulich@bitbucket.org>
2021-04-28 21:21:57 -0400

    Shortened interop.js by hundreds of lines

---
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/Entities/GraphData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaychexDataConsolidationTool.Entities
+{
+    public class GraphData
+    {
+        public string EntityName { get; set; }
+        public int[,] EntityCounts { get; set; }
+        public string[] Dates { get; set; }
+        public string[] Entitys { get; set; }
+        public GraphData(string theEntityName, string[] theDates, string[] theEntities, int i, int j)
+        {
+            this.EntityName = theEntityName;
+            this.Entitys = theEntities;
+            this.EntityCounts = new int[i, j];
+            this.Dates = theDates;
+        }
+    }
+}
--- PaychexDataConsolidationTool/PaychexDataConsolidationTool/wwwroot/js/interop.js
+++ PaychexDataConsolidationTool/PaychexDataConsolidationTool/wwwroot/js/interop.js
@@ -1,113 +1,5 @@
-﻿// -- STATUS
-function generateCPSGraph(graphdata) {
-    
-    var canvas = document.getElementById("lineGraph"); // Referencing the ID of the <canvas> tag in our razor file
-    var ctx = canvas.getContext('2d'); // This is a 2d graph
-    //Default font options
-    Chart.defaults.global.defaultFontColor = 'black'; 
-    Chart.defaults.global.defaultFontSize = 16;
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Ensuring the Y axist scale does not show up in 
-    //  'Scientific format' i.e (2E5), uses human readable
-    //  decimal values instead
-    //
-    Chart.scaleService.updateScaleDefaults('logarithmic', {
-        ticks: {
-            callback: function (tick, index, ticks) {
-                return tick.toLocaleString()
-            }
-        }
-    });
-    graphdata = JSON.parse(graphdata); // Parsing our serialized json object from our FetchCPS.razor file
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Data object to encapsulate x axis, and y axis data
-    //
-    var data = {
-        labels: graphdata.Dates,
-        datasets: []
-    };
-
-    ///////////////////////////////////////////////////////////////
-    //
-    //  Creating an object per status, each with randomized color
-    //  also associates corrensponding data with its status
-    //
-    var i = 0; // to reference index of two dimensional array
-    graphdata.Statuses.forEach(status => {
-        var randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-        var r = randomBetween(0, 255);
-        var g = randomBetween(0, 255);
-        var b = randomBetween(0, 255);
-        var rgb = `rgb(${r},${g},${b})`;
-        var obj = {
-            label: status,
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(225,0,0,0.4)",
-            borderColor: rgb, // The main line color
-            borderCapStyle: 'square',
-            borderDash: [], // try [5, 15] for instance
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "black",
-            pointBackgroundColor: "white",
-            pointBorderWidth: 1,
-            pointHoverRadius: 8,
-            pointHoverBackgroundColor: "yellow",
-            pointHoverBorderColor: "brown",
-            pointHoverBorderWidth: 2,
-            pointRadius: 4,
-            pointHitRadius: 10,
-            data: graphdata.StatusCounts[i],
-            spanGaps: true,
-        }
-        i++;
-        data.datasets.push(obj);
-    });
-
-    //////////////////////////////////////////////////////
-    //
-    //  Options for customizing our scales
-    //
-    var options = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                type: 'logarithmic',
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Count',
-                    fontSize: 20
-                }
-            }]
-        }
-    };
-
-    // Chart declaration (creating the chart with our data and options objects)
-    if (window.myLineChart != undefined) {
-        window.myLineChart.destroy();
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        })
-    } else {
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        });
-    }
-    return true; 
-}
-//--Client TYPE
-function generateCPTGraph(graphdata) {
+﻿//--ClientsPerStatus, ClientsPerType, UsersPerType, ClientsPerBrand Line Graphs
+function generateLineGraph(graphdata) {
 
     var canvas = document.getElementById("lineGraph"); // Referencing the ID of the <canvas> tag in our razor file
     var ctx = canvas.getContext('2d'); // This is a 2d graph
@@ -128,7 +20,7 @@ function generateCPTGraph(graphdata) {
             }
         }
     });
-    graphdata = JSON.parse(graphdata); // Parsing our serialized json object from our FetchCPS.razor file
+    graphdata = JSON.parse(graphdata); // Parsing our serialized json object from our FetchCPS/CPT/UPT etc.razor file
 
     ////////////////////////////////////////////////////////
     //
@@ -145,14 +37,14 @@ function generateCPTGraph(graphdata) {
     //  also associates corrensponding data with its type
     //
     var i = 0; // to reference index of two dimensional array
-    graphdata.Types.forEach(type => {
+    graphdata.Entitys.forEach(Entity => {
         var randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
         var r = randomBetween(0, 255);
         var g = randomBetween(0, 255);
         var b = randomBetween(0, 255);
         var rgb = `rgb(${r},${g},${b})`;
         var obj = {
-            label: type,
+            label: Entity,
             fill: false,
             lineTension: 0.1,
             backgroundColor: "rgba(225,0,0,0.4)",
@@ -170,116 +62,7 @@ function generateCPTGraph(graphdata) {
             pointHoverBorderWidth: 2,
             pointRadius: 4,
             pointHitRadius: 10,
-            data: graphdata.TypeCounts[i],
-            spanGaps: true,
-        }
-        i++;
-        data.datasets.push(obj);
-    });
-
-    //////////////////////////////////////////////////////
-    //
-    //  Options for customizing our scales
-    //
-    var options = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                type: 'logarithmic',
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Count',
-                    fontSize: 20
-                }
-            }]
-        }
-    };
-
-    // Chart declaration (creating the chart with our data and options objects)
-    if (window.myLineChart != undefined) {
-        window.myLineChart.destroy();
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        })
-    } else {
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        });
-    }
-    return true;
-}
-
-//--UserTYPE
-function generateUPTGraph(graphdata) {
-
-    var canvas = document.getElementById("lineGraph"); // Referencing the ID of the <canvas> tag in our razor file
-    var ctx = canvas.getContext('2d'); // This is a 2d graph
-    //Default font options
-    Chart.defaults.global.defaultFontColor = 'black';
-    Chart.defaults.global.defaultFontSize = 16;
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Ensuring the Y axist scale does not show up in 
-    //  'Scientific format' i.e (2E5), uses human readable
-    //  decimal values instead
-    //
-    Chart.scaleService.updateScaleDefaults('logarithmic', {
-        ticks: {
-            callback: function (tick, index, ticks) {
-                return tick.toLocaleString()
-            }
-        }
-    });
-    graphdata = JSON.parse(graphdata); // Parsing our serialized json object from our FetchCPS.razor file
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Data object to encapsulate x axis, and y axis data
-    //
-    var data = {
-        labels: graphdata.Dates,
-        datasets: []
-    };
-
-    ///////////////////////////////////////////////////////////////
-    //
-    //  Creating an object per status, each with randomized color
-    //  also associates corrensponding data with its type
-    //
-    var i = 0; // to reference index of two dimensional array
-    graphdata.Types.forEach(type => {
-        var randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-        var r = randomBetween(0, 255);
-        var g = randomBetween(0, 255);
-        var b = randomBetween(0, 255);
-        var rgb = `rgb(${r},${g},${b})`;
-        var obj = {
-            label: type,
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(225,0,0,0.4)",
-            borderColor: rgb, // The main line color
-            borderCapStyle: 'square',
-            borderDash: [], // try [5, 15] for instance
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "black",
-            pointBackgroundColor: "white",
-            pointBorderWidth: 1,
-            pointHoverRadius: 8,
-            pointHoverBackgroundColor: "yellow",
-            pointHoverBorderColor: "brown",
-            pointHoverBorderWidth: 2,
-            pointRadius: 4,
-            pointHitRadius: 10,
-            data: graphdata.TypeCounts[i],
+            data: graphdata.EntityCounts[i],
             spanGaps: true,
         }
         i++;
@@ -304,114 +87,6 @@ function generateUPTGraph(graphdata) {
                 }
             }]
         },
-    };
-
-    // Chart declaration (creating the chart with our data and options objects)
-    if (window.myLineChart != undefined) {
-        window.myLineChart.destroy();
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        })
-    } else {
-        window.myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: data,
-            options: options
-        });
-    }
-    return true;
-}
-
-function generateCPBGraph(graphdata) {
-
-    var canvas = document.getElementById("lineGraph"); // Referencing the ID of the <canvas> tag in our razor file
-    var ctx = canvas.getContext('2d'); // This is a 2d graph
-    //Default font options
-    Chart.defaults.global.defaultFontColor = 'black';
-    Chart.defaults.global.defaultFontSize = 16;
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Ensuring the Y axist scale does not show up in 
-    //  'Scientific format' i.e (2E5), uses human readable
-    //  decimal values instead
-    //
-    Chart.scaleService.updateScaleDefaults('logarithmic', {
-        ticks: {
-            callback: function (tick, index, ticks) {
-                return tick.toLocaleString()
-            }
-        }
-    });
-    graphdata = JSON.parse(graphdata); // Parsing our serialized json object from our FetchCPS.razor file
-
-    ////////////////////////////////////////////////////////
-    //
-    //  Data object to encapsulate x axis, and y axis data
-    //
-    var data = {
-        labels: graphdata.Dates,
-        datasets: []
-    };
-
-    ///////////////////////////////////////////////////////////////
-    //
-    //  Creating an object per status, each with randomized color
-    //  also associates corrensponding data with its status
-    //
-    var i = 0; // to reference index of two dimensional array
-    graphdata.Brands.forEach(brand => {
-        var randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-        var r = randomBetween(0, 255);
-        var g = randomBetween(0, 255);
-        var b = randomBetween(0, 255);
-        var rgb = `rgb(${r},${g},${b})`;
-        var obj = {
-            label: brand,
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(225,0,0,0.4)",
-            borderColor: rgb, // The main line color
-            borderCapStyle: 'square',
-            borderDash: [], // try [5, 15] for instance
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "black",
-            pointBackgroundColor: "white",
-            pointBorderWidth: 1,
-            pointHoverRadius: 8,
-            pointHoverBackgroundColor: "yellow",
-            pointHoverBorderColor: "brown",
-            pointHoverBorderWidth: 2,
-            pointRadius: 4,
-            pointHitRadius: 10,
-            data: graphdata.BrandCounts[i],
-            spanGaps: true,
-        }
-        i++;
-        data.datasets.push(obj);
-    });
-
-    //////////////////////////////////////////////////////
-    //
-    //  Options for customizing our scales
-    //
-    var options = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                },
-                type: 'logarithmic',
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Count',
-                    fontSize: 20
-                }
-            }]
-        }
     };
 
     // Chart declaration (creating the chart with our data and options objects)
